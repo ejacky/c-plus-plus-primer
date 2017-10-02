41d1087cf52a8125e7921b9124806277a3e63722
ejacky <ejacky001@gmail.com>
2017-10-02 08:38:39 +0800

    update

--- ForFudan/chapter5.cpp
+++ ForFudan/chapter5.cpp
@@ -3,5 +3,48 @@ using namespace std;
 
 class object 
 {
-	
+	private:
+		int val;
+	public:
+		object(): val(0)
+		{
+			cout << "Default constructor for object" << endl;
+		}
+		object(int i):val(i)
+		{
+			cout << "Constructor for object" << val << endl;
+		}
+		~object()
+		{
+			cout << "Destructor for object" << val << endl;
+		}
+};
+
+class container
+{
+	private:
+		object one;
+		object two;
+		int data;
+	public:
+		container():data(0)
+		{
+			cout << "Default construct for container" << endl;
+		}
+		container(int i, int j, int k);
+		~container()
+		{
+			cout << "Destructor for containter" << data << endl;
+		}
+};
+
+container::container(int i, int j, int k): two(i), one(j)
+{
+	data = k;
+	cout << "Constructor for container" << data << endl;
+}
+
+int main()
+{
+	container obj, anObj(5, 6, 10);
 }
