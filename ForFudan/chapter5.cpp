bcb9d59d8d28e46492bcbd3e7b89238e13891b13
ejacky <ejacky001@gmail.com>
2017-10-03 10:32:00 +0800

    update

--- ForFudan/chapter5.cpp
+++ ForFudan/chapter5.cpp
@@ -1,4 +1,21 @@
+class Test
+{
+	static int x;
+	int n;
+	public:
+		Test() {}
+		Test(int a, int b)
+		{
+			x =a;
+			n =b;	
+		}	
+		static int func() {return x;}
+		static void sfunc(Test &r, int a) {r.n = a;}
+		int Getn() {return n;}
+};
+int Test::x = 25;
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 class object 
@@ -44,7 +61,114 @@ container::container(int i, int j, int k): two(i), one(j)
 	cout << "Constructor for container" << data << endl;
 }
 
+class test1
+{
+	private:
+		int n;
+	public:
+		test1(int i)
+		{
+			n = i;
+			cout << "construct:" << i << endl;	
+		}	
+		~test1()
+		{
+			cout << "destrctor:" << n << endl;
+		}
+		int getn() {return n;}
+		void inc() {++n;}
+}; 
+
+class Point
+{
+	private:
+		double X, Y;
+	public:
+		Point(double xi, double yi) {X = xi; Y = yi;}
+		double GetX() {return X;}
+		double GetY() {return Y;}
+		friend double distances(Point &, Point &);
+};
+
+double distances(Point &a, Point &b)   // just like normal function
+{
+	double dx = a.X - b.X;
+	double dy = a.Y - b.Y;
+	return sqrt(dx * dx + dy * dy);
+}
+
+class Two;
+class One
+{
+	private:
+		int x;
+	public:
+		One(int a) {x = a;}
+		int GetX() {return x;}
+		void func(Two &);
+};
+
+class Two
+{
+	private:
+		int y;
+	public:
+		Two(int b) { y = b;}
+		int Gety() {return y;}
+		friend void One::func(Two &);
+};
+
+void One::func(Two & r)
+{
+	r.y = x;
+}
+
 int main()
 {
-	container obj, anObj(5, 6, 10);
+	//container obj, anObj(5, 6, 10);
+	
+	/**
+	* static member
+	*/
+//	cout << Test::func();
+//	Test b, c;
+//	b.sfunc(b, 58);
+//	cout << " " << b.Getn();
+//	cout << " " << b.func();
+//	cout << " " << c.func();
+//	Test a(24, 56);
+//	cout << " " << a.func() << " " << b.func() << " " << b.func() << endl;
+
+	/**
+	* static object
+	*/
+//	cout << "loop start" << endl;
+//	for (int i = 0; i < 3; i++) {
+//		static test1 a(3);
+//		test1 b(3);
+//		a.inc();
+//		b.inc();
+//		cout << "a.n=" << a.getn() << endl;
+//		cout << "b.n=" << b.getn() << endl;
+//	}
+//	
+//	cout << "loop end." << endl;
+//	cout << "Exit main()" << endl;
+
+	/**
+	*  friend function of class self 
+	*/
+//	Point p1(3.5, 5.5), p2(4.5, 6.5);
+//	cout << "The distance is " << distances(p1, p2) << endl;
+
+	/**
+	*	another class as friend function
+	*/
+	One Obj1(5);
+	Two Obj2(8);
+	cout << Obj1.GetX() << " " << Obj2.Gety() << endl;
+	Obj1.func(Obj2);
+	cout << Obj1.GetX() << " " << Obj2.Gety() << endl;
+
+
 }
