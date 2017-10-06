90caeeed12705c2693883a7614225be8a334b711
ejacky <ejacky001@gmail.com>
2017-10-06 19:30:49 +0800

    update

--- ForFudan/equation_main.cpp
+++ ForFudan/equation_main.cpp
@@ -1,1 +1,28 @@
 #include "equation.h"
+void Read(float &, float &, float &);
+int main(void)
+{
+	float a, b, c;
+	cout << "this is ax^2 + bx +c = 0" << endl;
+	for (;;) {
+		Read(a, b, c);
+		if (a == 0) return -1;
+		FindRoot obj(a, b, c);
+		obj.Find();
+		obj.Display();
+	}
+}
+
+void Read(float &a, float&b, float&c)
+{
+	cout << "input a:" ;
+	cin >> a;
+	if (a == 0) {
+		getchar();
+		return;
+	}
+	cout << "input b:";
+	cin >> b;
+	cout << "input c:";
+	cin >> c;
+}
